47ea48fe45bc36bd9a36366fdfa921642f10387b
guei10473 <guei10473@gmail.com>
2023-05-04 06:18:56 +0800

    5/4 commit

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
+  const [input, setInput] = useState("");
+  let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "2HbarqkSJnfPYiy9cKuOYmuUDXSTloefv9yeuL3lEAab29nmuPxUiJNK";
+  const intialURL = "https://api.pexels.com/v1/curated?page=1per_page=15";
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
+
+  // fetch data from pexels api
+  const search = async (url) => {
+    const dataFetch = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "applictaion/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch.json();
+    setData(parseData.photos);
+  };
+
+  // load more picture
+  const morepicture = async () => {
+    setPage(2);
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "applictaion/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch.json();
+    setData(data.concat(parseData.photos));
+  };
+
+  // fetch data when the page loads up
+  useEffect(() => {
+    search(intialURL);
+  }, []);
+
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(intialURL);
+    } else search(searchURL);
+    search(searchURL);
+  }, [currentSearch]);
+
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search />
-      <h1>This is hompage.</h1>
+      <Search
+        search={() => {
+          setCurrentSearch(input);
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More</button>
+      </div>
     </div>
   );
 };
